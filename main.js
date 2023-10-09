611485a6f9bbe0153657022064d5e2b425aa5c0f
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-09 17:51:48 +0800

    added secondary mid section

--- main.js
+++ main.js
@@ -9,66 +9,78 @@ const imgwrapper = document.querySelector('.imgwrap');
 const marker = document.querySelectorAll('.marker')
 const marquee = document.querySelector(".marquee")
 const wrap = document.querySelector(".scrollwrap")
+const accord = document.querySelector(".accordion_middle");
+const accordimage = document.querySelector(".accordion_image")
+const accordinner = document.querySelectorAll(".accordion_inner")
 
 const scrollOpts = {
-    target: imgwrapper,
-    offset: ["start end", "end end"]
+  target: imgwrapper,
+  offset: ["start end", "end end"]
 }
 
 
 inView(imgwrap, () => {
-    animate('h2 span', { opacity: [0.2, 1], rotateX: ["90deg", 0] }, { delay: stagger(0.1, { from: "first" }) })
-    animate('.imgwrapper', { opacity: [0, 1], translateY: [-1000, 0] }, { duration: 2, easing: 'ease' })
+  animate('h2 span', { opacity: [0.2, 1], rotateX: ["90deg", 0] }, { delay: stagger(0.1, { from: "first" }) })
+  animate('.imgwrapper', { opacity: [0, 1], translateY: [-1000, 0] }, { duration: 2, easing: 'ease' })
 })
 
+// for (let i = 0; i < accordinner.length; i++) {
+//   accordinner[i].addEventListener('mouseover', () => {
+//     animate(accordinner[i], { height: '50vh' }, { easing: "linear" })
+//   })
+//   accordinner[i].addEventListener('mouseleave', () => {
+//     animate(accordinner[i], { height: "auto" })
+//   })
+// }
+
 scroll(
-    animate(".software", { x: [0, "-100vw"] })
+  animate(".software", { x: [0, "-100vw"] })
 )
 scroll(
-    animate(".engineer", { x: [0, "100vw"] })
+  animate(".engineer", { x: [0, "100vw"] })
 )
 scroll(
-    animate(".mainimage", { y: [0, "50vh"], scale: [1, 2], opacity: [1, 0] })
+  animate(".mainimage", { y: [0, "50vh"], scale: [1, 2], opacity: [1, 0] })
 )
 
 scroll(
-    animate('.innerimage', { y: [0, "-50vh"] }, { delay: 0.5 })
+  animate('.innerimage', { y: [0, "-50vh"] }, { delay: 0.5 })
 )
 
 
-document.addEventListener('mousemove', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
+document.addEventListener('mousemove', function(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
 });
 for (let i = 0; i < marker.length; i++) {
 
-    marker[i].addEventListener('mouseover', () => {
-        animate(cursor, { scale: 4.5, opacity: 0.3 }, { easing: spring() })
-    })
+  marker[i].addEventListener('mouseover', () => {
+    animate(cursor, { scale: 4.5, opacity: 0.3 }, { easing: spring() })
+  })
 
-    marker[i].addEventListener('mouseleave', () => {
-        animate(cursor, { scale: 1, opacity: 0.6 }, { easing: spring() })
-    })
+  marker[i].addEventListener('mouseleave', () => {
+    animate(cursor, { scale: 1, opacity: 0.6 }, { easing: spring() })
+  })
 }
 
 animate(marquee, { x: "-50%" }, { duration: 10, repeat: Infinity, easing: "linear" })
 
-document.addEventListener('mouseleave', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.transform = x + 'px';
-    cursor.style.transform = y + 'px';
-    animate(cursor, { scale: 1, opacity: 0 }, { duration: 0.5, ease: "ease-in-out" })
+document.addEventListener('mouseleave', function(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.transform = x + 'px';
+  cursor.style.transform = y + 'px';
+  animate(cursor, { scale: 1, opacity: 0 }, { duration: 0.5, ease: "ease-in-out" })
 });
 
-document.addEventListener('mouseenter', function (e) {
-    var x = e.clientX;
-    var y = e.clientY;
-    cursor.style.left = x + 'px';
-    cursor.style.top = y + 'px';
-    animate(cursor, { scale: 1, opacity: 1 }, { duration: 0.5, ease: "ease-in-out" })
+document.addEventListener('mouseenter', function(e) {
+  var x = e.clientX;
+  var y = e.clientY;
+  cursor.style.left = x + 'px';
+  cursor.style.top = y + 'px';
+  animate(cursor, { scale: 1, opacity: 1 }, { duration: 0.5, ease: "ease-in-out" })
 });
 
 imgwrap.addEventListener('mousemove', imgMouse)
@@ -76,20 +88,25 @@ imgwrap.addEventListener('mouseleave', imgReset)
 
 function imgMouse(e) {
 
-    const { top, left, width, height } = imgwrap.getBoundingClientRect();
-    const centerX = left + width / 2;
-    const centerY = top + height / 2;
-    const mouseX = e.clientX - centerX;
-    const mouseY = e.clientY - centerY;
-    const rotateX = 20 * (mouseX / (height / 2));
-    const rotateY = 20 * (mouseY / (width / 2));
+  const { top, left, width, height } = imgwrap.getBoundingClientRect();
+  const centerX = left + width / 2;
+  const centerY = top + height / 2;
+  const mouseX = e.clientX - centerX;
+  const mouseY = e.clientY - centerY;
+  const rotateX = 20 * (mouseX / (height / 2));
+  const rotateY = 20 * (mouseY / (width / 2));
 
-    animate(".imgwrap", { rotateX: -1 * rotateY, rotateY: rotateX }, { easing: spring() })
+  animate(".imgwrap", { rotateX: -1 * rotateY, rotateY: rotateX }, { easing: spring() })
 
 }
 
 function imgReset() {
-    animate(".imgwrap", { rotateX: 0, rotateY: 0 }, { easing: spring() })
+  animate(".imgwrap", { rotateX: 0, rotateY: 0 }, { easing: spring() })
 }
+
+inView(accordinner[1], () => {
+  animate(accordimage, { x: [-500, 0] }, { duration: 1, easing: spring() })
+})
+
 
 
--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,11 @@
 // uno.config.ts
 import { defineConfig, presetWebFonts } from 'unocss'
+import { transformerVariantGroup } from 'unocss'
 
 
 export default defineConfig({
   // ...UnoCSS options
+  transformers: [
+    transformerVariantGroup(),
+  ]
 })
