ea1409b5db26c34ebaf4ec6e8413e388b6f0923e
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-12 17:13:51 +0800

    first comm

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 // vite.config.ts
 import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
+import htmlTemplate from 'vite-plugin-html-template'
+
 
 export default defineConfig({
   plugins: [
     UnoCSS(),
+    htmlTemplate(
+      {
+        // where is the pages' root directory?
+        pagesDir: 'src/pages',
+        // define pages like it is done in vue-cli
+        pages: {
+          index: {
+            template: './src/pages/index.html',
+            title: 'Homepage',
+          },
+          about: {
+            template: './src/pages/about.html',
+            title: 'About',
+          },
+          project: {
+            template: './src/pages/projects.html',
+            title: 'Project',
+          }
+        },
+        // expose to template
+        data: {
+          title: 'Homepage',
+        },
+      }
+    ),
+    
   ],
 })
